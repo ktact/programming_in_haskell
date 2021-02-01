a35c57282f021ce89825457160b8b4dde74fccc5
K Tact <ktact.dev@gmail.com>
2021-02-01 20:41:02 +0900

    2.5.5 コメントの例にならいtest.hsにコメントを追加

--- chapter2/test.hs
+++ chapter2/test.hs
@@ -1,4 +1,8 @@
 double x = x + x
 quadruple x = double (double x)
+
+-- Factorial of a positive integer:
 factorial n = product [1..n]
+
+-- Average of a list of integers:
 average ns = sum ns `div` length ns
