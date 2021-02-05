82fd516ab2cdb38d98dc3b54f0751481a43c2c36
K Tact <ktact.dev@gmail.com>
2021-02-05 21:54:25 +0900

    5.7 練習問題の5に回答

--- chapter5/exercises.hs
+++ chapter5/exercises.hs
@@ -7,3 +7,7 @@ grid m n = [(x,y) | x <- [0..m], y <- [0..n]]
 -- 4
 replicate :: Int -> a -> [a]
 replicate n x = [x | _ <- [1..n]]
+
+-- 5
+pyths :: Int -> [(Int,Int,Int)]
+pyths n = [(x,y,z) | x <- [1..n], y <- [1..n], z <- [1..n], z^2 == x^2 + y^2]
