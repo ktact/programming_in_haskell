731357aa5714f7a632147f91115a9b824e949543
K Tact <ktact.dev@gmail.com>
2021-02-06 22:18:44 +0900

    6.8 練習問題の4に回答

--- chapter6/exercises.hs
+++ chapter6/exercises.hs
@@ -7,3 +7,9 @@ sumdown n = n + sumdown (n-1)
 (^) :: Int -> Int -> Int
 _ ^ 0 = 1
 n ^ m = n * (n ^ (m-1))
+
+-- 4
+euclid :: Int -> Int -> Int
+euclid x y | x == y = x
+           | x <  y = euclid x (y - x)
+           | x >  y = euclid y (x - y)
